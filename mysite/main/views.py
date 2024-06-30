34043969a919fe0ab758021fd795c3bd0989bf5d
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-06-30 18:20:27 +0300

    4 page

---
+++ mysite/main/views.py
@@ -0,0 +1,17 @@
+# main/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+from django.shortcuts import render
+
+# Create your views here.
