4f5fe6b7e8db377186908cb913521276f2c299f7
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-07-02 11:09:41 +0300

    news

---
+++ mysite/news/urls.py
@@ -0,0 +1,8 @@
+# news/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.news_list, name='news_list'),
+    path('<int:news_id>/', views.news_detail, name='news_detail'),
+]
